e75ca942d99f1168d78bbbc0af7ba7a33b851ea7
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-09-01 18:05:22 +0600

    Added log config based on levels

---
+++ config.mjs
@@ -0,0 +1,40 @@
+export default {
+    levels: {
+        access: {
+            color: 'red',
+            writeAccess: true
+        },
+        warn: {
+            color: 'green',
+            writeAccess: true
+        },
+        debug: {
+            color: 'yellow',
+            writeAccess: true
+        },
+        system: {
+            color: 'cyan',
+            writeAccess: true
+        },
+        database: {
+            color: 'magenta',
+            writeAccess: true
+        },
+        event: {
+            color: 'whiteBright',
+            writeAccess: true
+        },
+        info: {
+            color: 'blueBright',
+            writeAccess: true
+        },
+        error: {
+            color: 'grey',
+            writeAccess: true
+        },
+        fatal: {
+            color: 'redBright',
+            writeAccess: true
+        }
+    }
+}
