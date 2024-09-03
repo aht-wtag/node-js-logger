ca58bd9656225cd2ecc0d94bfa65c67ce7547457
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-09-03 14:06:04 +0600

    added writing log file to a file functionality

--- logger.mjs
+++ logger.mjs
@@ -1,16 +1,19 @@
 import path from 'path'
 import chalk from 'chalk'
 import moment from 'moment'
-import { existsSync, mkdirSync, appendFileSync, createReadStream } from 'fs'
+import { existsSync, mkdirSync, appendFileSync, createReadStream, write } from 'fs'
 import config from './config.mjs'
 import readLine from 'readline'
 
 export const log = (options) => {
     const levelName = getLevelName(options.level);
     let message = options.message ?? 'Unidentified Error';
-    const error = options.error ?? null ;
+    const error = options.error ?? null;
+    writeToConsole(levelName, message);
 
-    writeToConsole(levelName, message, error)
+    if(config.levels[levelName].writeAccess) {
+        writeToFile(levelName, message, error);
+    }
 }
 
 const writeToConsole = (levelName, message, error= null) => {
@@ -25,6 +28,22 @@ const writeToConsole = (levelName, message, error= null) => {
 
     console.log(`[${chalkFunc(header)}] : [${message}]`);
 
+}
+
+const writeToFile = (level, message) => {
+    const logDirectory = "./logs";
+
+    const logData = `{"level" : "${level.toUpperCase()}", "message": "${message}", "time" : "${dateFormat()}"}\r\n`;
+    if(!existsSync(logDirectory)) {
+        mkdirSync(logDirectory);
+    }
+
+    const options = {
+        encoding : 'utf8',
+        mode: 438
+    }
+
+    appendFileSync(`./logs/${level}.log`,logData, options);
 }
 
 const getLevelName = (level) => {
